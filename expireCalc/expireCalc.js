b0163a97e6d8dfba6f1381b7f541df8d22f54b39
Darren Reed <dlreed1@cai0l906223.man.co>
2017-06-21 00:19:08 -0400

    numerous bug fixes and tests

--- expireCalc/expireCalc.js
+++ expireCalc/expireCalc.js
@@ -1,10 +1,42 @@
-const expireOnDate = (date) => {
+const moment = require('moment');
 
+const convertToMs = (momentDate) => {
+    return momentDate.toDate().getTime();
 };
 
-const expireWithTimeUnit = (numberOfUnits, units) => {
-    // parse which units:
-    // convert to seconds, minimum 1 (0 will never expire)
+const expireWithTimeUnit = (units, timeUnit = 'seconds') => {
+    if (!units || units <= 0) {
+        return -1;
+    }
+
+    const timeUnitUsed = moment.normalizeUnits(timeUnit) || 'seconds';
+    const duration = moment.duration(units, timeUnitUsed);
+    if (duration.isValid()) {
+        const result = Math.ceil(duration.asSeconds());
+        if (result > 0) {
+            return result;
+        }
+    }
+
+    return -1;
+};
+
+const expireOnDate = (date) => {
+    const parsedUtcDate = moment.utc(date);
+
+    // needs to be a valid moment date
+    if (!parsedUtcDate.isValid()) {
+        return -1;
+    }
+
+    const secondsFromNow = convertToMs(parsedUtcDate) - convertToMs(moment.utc());
+
+    // difference must be larger than 0 in order to set the correct time;
+    if (secondsFromNow > 0) {
+        return expireWithTimeUnit(secondsFromNow, 'ms'); // convert to seconds
+    }
+
+    return -1;
 };
 
 module.exports = {
---
+++ test/expireCalc.test.js
@@ -0,0 +1,101 @@
+const Code = require('code');
+const Lab = require('lab');
+const moment = require('moment');
+
+const lab = Lab.script();
+exports.lab = lab;
+
+const toTest = require('../expireCalc/expireCalc');
+
+
+lab.experiment('expireCalc tests', () => {
+    lab.experiment('expireOnDate', () => {
+        lab.test('should return a negative number (to prevent caching) if an invalid type is sent', (finish) => {
+            Code.expect(toTest.expireOnDate()).to.equal(-1);
+            Code.expect(toTest.expireOnDate(null)).to.equal(-1);
+            Code.expect(toTest.expireOnDate({})).to.equal(-1);
+            Code.expect(toTest.expireOnDate(false)).to.equal(-1);
+            Code.expect(toTest.expireOnDate(true)).to.equal(-1);
+            Code.expect(toTest.expireOnDate('')).to.equal(-1);
+            finish();
+        });
+
+        lab.test('should handle negative numbers (This will evaluate to a time in the past which results in -1).', (finish) => {
+            Code.expect(toTest.expireOnDate(-100)).to.equal(-1);
+            finish();
+        });
+
+        lab.test('should handle dates in the past', (finish) => {
+            Code.expect(toTest.expireOnDate(Date.now() - 1000)).to.equal(-1);
+            finish();
+        });
+
+        lab.test('should return a valid number of seconds from any timezone (convert to UTC and then expire)', (finish) => {
+            const futureTime = moment().add(1, 'h');
+            const expected = 60 * 60; // 60 mins in seconds
+            const actual = toTest.expireOnDate(futureTime);
+
+            Code.expect(actual).to.equal(expected);
+            finish();
+        });
+
+        lab.test('should be able to handle a target date (1st of next month)', (finish) => {
+            const now = new Date();
+            const currYear = now.getFullYear();
+            const currMonth = now.getMonth() + 1; // convert to 1-based month for moment
+            const currDay = now.getDate();
+
+            const futureTime = moment(`${currYear}-${currMonth + 2}-${currDay}`);
+            const expected = Math.ceil((futureTime.toDate() - moment.utc().toDate()) / 1000);
+            const actual = toTest.expireOnDate(futureTime);
+
+            Code.expect(actual).to.equal(expected);
+            finish();
+        });
+    });
+
+    lab.experiment('expireWithTimeUnit', () => {
+        lab.test('should return -1 if passed a negative number', (finish) => {
+            Code.expect(toTest.expireWithTimeUnit(-30)).to.equal(-1);
+            finish();
+        });
+
+        lab.test('should return -1 if passed 0 (0 means never expire - that is what the set commad is for)', (finish) => {
+            Code.expect(toTest.expireWithTimeUnit(0)).to.equal(-1);
+            finish();
+        });
+
+        lab.test('should return -1 if the duration is invalid', (finish) => {
+            Code.expect(toTest.expireWithTimeUnit(-5, 'MEGASECONDS')).to.equal(-1);
+            finish();
+        });
+
+        lab.test('should not modify input due to invalid time unit', (finish) => {
+            Code.expect(toTest.expireWithTimeUnit(5, 'MEGASECONDS')).to.equal(5);
+            finish();
+        });
+
+        lab.test('should convert valid durations and timeunits correctly', (finish) => {
+            const FIVE_HOURS_IN_SECONDS = 5 * 60 * 60;
+            Code.expect(toTest.expireWithTimeUnit(5, 'h')).to.equal(FIVE_HOURS_IN_SECONDS);
+            Code.expect(toTest.expireWithTimeUnit(5, 'hours')).to.equal(FIVE_HOURS_IN_SECONDS);
+            Code.expect(toTest.expireWithTimeUnit(5, 'Hours')).to.equal(FIVE_HOURS_IN_SECONDS);
+            finish();
+        });
+
+        lab.test('should use seconds by default', (finish) => {
+            Code.expect(toTest.expireWithTimeUnit(100)).to.equal(100);
+            finish();
+        });
+
+        lab.test('should handle falsy or invalid types of data', (finish) => {
+            Code.expect(toTest.expireWithTimeUnit()).to.equal(-1);
+            Code.expect(toTest.expireWithTimeUnit(null)).to.equal(-1);
+            Code.expect(toTest.expireWithTimeUnit(false)).to.equal(-1);
+            Code.expect(toTest.expireWithTimeUnit(true)).to.equal(-1);
+            Code.expect(toTest.expireWithTimeUnit({})).to.equal(-1);
+            Code.expect(toTest.expireWithTimeUnit([])).to.equal(-1);
+            finish();
+        });
+    });
+});
