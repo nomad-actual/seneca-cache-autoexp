2b9eac464a31d659104923b99525ed088a6a86c0
Will-Clark <will.clark@coxautoinc.com>
2017-06-21 14:03:05 -0400

    Added test for expireInSeconds.

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function expireInSeconds(msg, done) {
         done(null); // return null as result of cache set
     }
 
-    cacheData(msg.key, msg.value, msg.expirationSeconds, done);
+    cacheData(msg.key, msg.value, msg.expirationSeconds, seneca, done);
 }
 
 /**
@@ -73,8 +73,7 @@ function expireOnDateWithTrustIssues(msg, done) {
     done(null); // always fail cache for now
 }
 
-function cacheData(key, value, time, done) {
-    const seneca = this;
+function cacheData(key, value, time, seneca, done) {
     const logger = seneca.log;
     logger.info({
         message: `Cache request: key: ${key} to expire in ${time} seconds`,
